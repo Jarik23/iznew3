d64b7817639d8041f339787189bba1bf1c6f1df5
Mishelle19 <84457541+Mishelle19@users.noreply.github.com>
2021-06-13 21:52:45 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -62,13 +62,13 @@ def draw(filename,cho):
  plt.savefig(gr_path)
  plt.close()
  
- import scipy.ndimage.interpolation as interp
-image_rot = interp.rotate(input=image_copy, angle=45, axes=(0,1), reshape = False)
+ #import scipy.ndimage.interpolation as interp
+#image_rot = interp.rotate(input=image_copy, angle=45, axes=(0,1), reshape = False)
  
- #img=img.rotate(cho)
+ img=img.rotate(cho)
  output_filename = filename
  img.save(output_filename)
- 
+ return output_filename,gr_path
 
 @app.route("/net",methods=['GET', 'POST'])
 def net():
